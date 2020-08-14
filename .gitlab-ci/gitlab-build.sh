8919fad3d7e1b642e41f2c05470565737f1b5490
Mario Konrad <mario.konrad@gmx.net>
2020-08-14 19:29:10 +0200

    Add GitLab-CI configuration

---
+++ .gitlab-ci/gitlab-build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu
+
+function section()
+{
+	id=$1
+	shift
+
+	echo -e "section_start:`date +%s`:${id}\r\e[0K${id}"
+	eval $@
+	echo -e "section_end:`date +%s`:${id}\r\e[0K"
+}
+
+section configure cmake -B build -DCMAKE_BUILD_TYPE=${BUILD_TYPE} .
+section build     cmake --build build -j
+section unittest  cmake --build build --target test -- ARGS="--verbose"
+
