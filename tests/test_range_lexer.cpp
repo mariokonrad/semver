17bc203ac3f16c479099dda0384f79f7af249f14
Mario Konrad <mario.konrad@gmx.net>
2020-08-15 14:13:52 +0200

    Preparations for lexer tests

---
+++ tests/test_range_lexer.cpp
@@ -0,0 +1,13 @@
+#include <semver/range.hpp>
+#include <gtest/gtest.h>
+
+namespace
+{
+using lexer = semver::detail::lexer;
+
+class test_range_lexer : public ::testing::Test {};
+
+// TODO: implementation
+
+}
+
