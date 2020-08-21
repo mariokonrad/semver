e146d148d8596bc4f3b336d961beda9c9d67d9f1
Mario Konrad <mario.konrad@gmx.net>
2020-08-21 17:30:19 +0200

    Test for transformation or-rotations

--- tests/test_range_node.cpp
+++ tests/test_range_node.cpp
@@ -328,5 +328,60 @@ TEST_F(test_range_node, transform_or_smaller_leaf_move_left)
 	EXPECT_EQ(expected_versions_before, visited_versions_before);
 	EXPECT_EQ(expected_versions_after, visited_versions_after);
 }
+
+TEST_F(test_range_node, transform_or_sort_leafs_across_or_nodes)
+{
+	// clang-format off
+	auto n = node::create_or(
+			std::make_unique<node>(node::create_or(
+				std::make_unique<node>(node::create_lt(semver("4.5.6"))),
+				std::make_unique<node>(node::create_gt(semver("3.4.5")))
+				)),
+			std::make_unique<node>(node::create_or(
+				std::make_unique<node>(node::create_le(semver("2.3.4"))),
+				std::make_unique<node>(node::create_ge(semver("1.2.3")))
+				))
+		);
+
+	const std::vector<node::type> expected_types_before = {
+		node::type::op_lt, node::type::op_gt, node::type::op_or,
+		node::type::op_le, node::type::op_ge, node::type::op_or,
+		node::type::op_or
+		};
+	const std::vector<semver> expected_versions_before = {
+		semver("4.5.6"), semver("3.4.5"), semver("2.3.4"), semver("1.2.3")
+		};
+	const std::vector<node::type> expected_types_after = {
+		node::type::op_ge, node::type::op_le, node::type::op_or,
+		node::type::op_gt, node::type::op_lt, node::type::op_or,
+		node::type::op_or
+		};
+	const std::vector<semver> expected_versions_after = {
+		semver("1.2.3"), semver("2.3.4"), semver("3.4.5"), semver("4.5.6")
+		};
+	// clang-format on
+
+	std::vector<node::type> visited_types_before;
+	std::vector<node::type> visited_types_after;
+	std::vector<semver> visited_versions_before;
+	std::vector<semver> visited_versions_after;
+
+	n.visit_postfix([&](const node & n) {
+		visited_types_before.push_back(n.get_type());
+		if (n.is_leaf())
+			visited_versions_before.push_back(*n.get_version());
+	});
+	n.optimize();
+	n.visit_postfix([&](const node & n) {
+		visited_types_after.push_back(n.get_type());
+		if (n.is_leaf())
+			visited_versions_after.push_back(*n.get_version());
+	});
+
+	EXPECT_EQ(expected_types_before, visited_types_before);
+	EXPECT_EQ(expected_types_after, visited_types_after);
+	EXPECT_EQ(expected_versions_before, visited_versions_before);
+	EXPECT_EQ(expected_versions_after, visited_versions_after);
+}
 }
 
