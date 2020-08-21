4a83dfe11b3668fa6d926dc05f6541e3cec134d6
Mario Konrad <mario.konrad@gmx.net>
2020-08-21 10:31:48 +0200

    Add tests for satisfies

--- tests/test_range_query.cpp
+++ tests/test_range_query.cpp
@@ -26,7 +26,75 @@ TEST_F(test_range_query, outside)
 	EXPECT_TRUE(r.outside(semver("3.1.0")));
 }
 
-TEST_F(test_range_query, satisfies)
+TEST_F(test_range_query, satisfies_greater_than)
+{
+	const auto r = range(">1.2.3");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_FALSE(r.satisfies(semver("1.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.2")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.3")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.4")));
+	EXPECT_TRUE(r.satisfies(semver("1.3.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.9.0")));
+	EXPECT_TRUE(r.satisfies(semver("2.0.0")));
+	EXPECT_TRUE(r.satisfies(semver("2.1.0")));
+	EXPECT_TRUE(r.satisfies(semver("3.0.0")));
+}
+
+TEST_F(test_range_query, satisfies_less_than)
+{
+	const auto r = range("<2.0.0");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_TRUE(r.satisfies(semver("1.0.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.2")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.3")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.4")));
+	EXPECT_TRUE(r.satisfies(semver("1.3.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.9.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
+	EXPECT_FALSE(r.satisfies(semver("3.0.0")));
+}
+
+TEST_F(test_range_query, satisfies_inbetween)
+{
+	const auto r = range(">1.2.3 <2.0.0");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_FALSE(r.satisfies(semver("1.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.2")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.3")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.4")));
+	EXPECT_TRUE(r.satisfies(semver("1.3.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.9.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
+	EXPECT_FALSE(r.satisfies(semver("3.0.0")));
+}
+
+TEST_F(test_range_query, satisfies_or)
+{
+	const auto r = range("1.2.3 || 2.0.0");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_FALSE(r.satisfies(semver("1.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.2")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.3")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.4")));
+	EXPECT_FALSE(r.satisfies(semver("1.3.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.9.0")));
+	EXPECT_TRUE(r.satisfies(semver("2.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
+	EXPECT_FALSE(r.satisfies(semver("3.0.0")));
+}
+
+TEST_F(test_range_query, satisfies_inbetween_with_or)
 {
 	const auto r = range(">1.2.3 <2.0.0 || 3.0.0");
 
@@ -42,6 +110,42 @@ TEST_F(test_range_query, satisfies)
 	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
 	EXPECT_TRUE(r.satisfies(semver("3.0.0")));
 }
+
+TEST_F(test_range_query, satisfies_hyphen)
+{
+	const auto r = range("1.2.3 - 2.0.0");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_FALSE(r.satisfies(semver("1.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.2")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.3")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.4")));
+	EXPECT_TRUE(r.satisfies(semver("1.3.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.9.0")));
+	EXPECT_TRUE(r.satisfies(semver("2.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
+	EXPECT_FALSE(r.satisfies(semver("3.0.0")));
+}
+
+TEST_F(test_range_query, satisfies_hyphen_with_or)
+{
+	const auto r = range("1.2.0 || 1.2.3 - 2.0.0");
+
+	ASSERT_TRUE(r.ok());
+	EXPECT_FALSE(r.satisfies(semver("1.0.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.0")));
+	EXPECT_FALSE(r.satisfies(semver("1.2.2")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.3")));
+	EXPECT_TRUE(r.satisfies(semver("1.2.4")));
+	EXPECT_TRUE(r.satisfies(semver("1.3.0")));
+	EXPECT_TRUE(r.satisfies(semver("1.9.0")));
+	EXPECT_TRUE(r.satisfies(semver("2.0.0")));
+	EXPECT_FALSE(r.satisfies(semver("2.1.0")));
+	EXPECT_FALSE(r.satisfies(semver("3.0.0")));
+}
+
+// TODO: satisfies with tilde, caret and xranges
 
 TEST_F(test_range_query, max)
 {
